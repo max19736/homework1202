30575f19a8713062ace9d5190386b84eca41d507
user <user@2-5>
2016-12-02 12:01:14 +0800

    second

--- WebApplication2/WebApplication1/Service/DataService.cs
+++ WebApplication2/WebApplication1/Service/DataService.cs
@@ -14,18 +14,33 @@ namespace WebApplication1.Service
 
             var item = new Models.Album();
 
-            item.Genre = "pop";
-            item.Title = "The Best Of The Men At Work";
+            item.Genre = "1103105306";
+            item.Title = "劉郁心";
             item.Price = 8;
 
             list.Add(item);
 
             var item2 = new Models.Album();
 
-            item2.Genre = "Metal";
-            item2.Title = "Black Light Syndrome";
+            item2.Genre = "1103105322";
+            item2.Title = "陳怡君";
             item2.Price = 20;
             list.Add(item2);
+
+            var item3 = new Models.Album();
+
+            item3.Genre = "1103105327";
+            item3.Title = "李偲瑋";
+            item3.Price = 20;
+            list.Add(item3);
+
+            var item4 = new Models.Album();
+
+            item4.Genre = "1103105344";
+            item4.Title = "林佑宗";
+            item4.Price = 20;
+            list.Add(item4);
+
             return list;
         }
 
